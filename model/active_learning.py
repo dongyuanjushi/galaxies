2cd9b86c954c45ee61cd1fb639c31caf1edb5804
梅凯 <181840164@smail.nju.edu.cn>
2021-03-01 19:12:09 +0800

    Active Learning draft

--- model/active_learning.py
+++ model/active_learning.py
@@ -1,44 +1,58 @@
 import pandas as pd
-from model.supervised_learning import build, predict
+from model.supervised_learning import build, predict, standard
+from sklearn.linear_model import SGDClassifier
+from sklearn import svm
 from sklearn.utils import shuffle
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 from util.select import balanced_select
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 batch = 100
 accuracy = []
-threshold = 0.90
+threshold = 0.96
 
 
 def train(train_data, val_data):
+    # train_data = standard(train_data)
     next_data = train_data[:batch]
     res_data = train_data[batch:]
+    # model = svm.SVC(probability=True, class_weight="balanced")
+    model= RandomForestClassifier(class_weight='balanced',max_depth=4)
+    strategy = "Least Confident"
     for k in range(1, len(train_data) // batch):
         print("Round {} is training!".format(k))
-        model = build(next_data, "SVM")
+        build(next_data, model)
         acc, pre_y, val_y = predict(res_data, model)
         score, _, _ = predict(val_data, model)
         accuracy.append(score)
         if score > threshold:
             break
-        next_data, res_data = query(res_data, pre_y, val_y)
         print("Round {} finished training!".format(k))
         print("Accuracy on the test data is %.4f" % score)
         print('-------------------------\n\n')
-        # print(next_data.head())
-        # next_data, res_data = query(res_data)
+        next_data, res_data = query(res_data, pre_y, val_y, strategy)
     acc_plot()
 
 
 def query(res_data, pre_y=None, val_y=None, strategy="random"):
     if strategy == "random":
+        # print(len(res_data))
         shuffled_data = shuffle(res_data)
         return shuffled_data[:batch], shuffled_data[batch:]
-    else:
-        return None
+    elif strategy == "Least Confident":
+        return least_confident(res_data, pre_y, val_y)
 
 
-def least_confident(res_data):
-    pass
+def least_confident(res_data, pre_y, val_y):
+    confident = np.max(pre_y, axis=1).reshape(len(pre_y), 1)
+    res_data = res_data.copy()
+    res_data["temp"] = confident
+    res_data = res_data.sort_values(by="temp", ascending=True)
+    return res_data[:batch], res_data[batch:]
 
 
 def acc_plot(strategy="random"):
@@ -51,9 +65,14 @@ def acc_plot(strategy="random"):
     plt.show()
 
 
-def execute(train_data_path, val_data_path):
+def plot_dis(x):
+    pass
+
+
+def a_execute(train_data_path, val_data_path=None):
     raw_train_data = pd.read_csv(train_data_path)
-    train_data = shuffle(balanced_select(raw_train_data, 5000 // 2))
+    train_data = raw_train_data
+    # train_data = shuffle(balanced_select(raw_train_data, 5000 // 2))
     # raw_val_data = pd.read_csv(val_data_path)
-    val_data = shuffle(balanced_select(raw_train_data, 2000 // 2))
+    train_data, val_data = train_data[:8000], train_data[8000:8100]
     train(train_data, val_data)
--- util/select.py
+++ util/select.py
@@ -5,20 +5,11 @@ import cv2
 import numpy as np
 
 classified_data = ['Error',
-                   # 'ML2classes',
                    'CNN2classes1stClass', 'CNN2classes1stClassPerc',
                    'CNN2classes2ndClass', 'CNN2Classes2ndClassPerc', 'CNN3classes1stClass',
                    'CNN3Classes1stClassPerc', 'CNN3Classes2ndClass',
                    'CNN3Classes2ndClassPerc', 'CNN3Classes3rdClass',
                    'CNN3Classes3rdClassPerc']
-
-drop_list = ['Class2.1', 'Class2.2',
-             'Class3.1', 'Class3.2', 'Class4.1', 'Class4.2', 'Class5.1', 'Class5.2',
-             'Class5.3', 'Class5.4', 'Class6.1', 'Class6.2', 'Class7.1', 'Class7.2',
-             'Class7.3', 'Class8.1', 'Class8.2', 'Class8.3', 'Class8.4', 'Class8.5',
-             'Class8.6', 'Class8.7', 'Class9.1', 'Class9.2', 'Class9.3', 'Class10.1',
-             'Class10.2', 'Class10.3', 'Class11.1', 'Class11.2', 'Class11.3',
-             'Class11.4', 'Class11.5', 'Class11.6']
 
 
 def read(url):
@@ -29,77 +20,34 @@ def read(url):
     return train_csv[~train_csv["Class"].isin(['U', '--'])]
 
 
-def process(url):
-    train_data = pd.read_csv(url, low_memory=False).drop(columns=drop_list)
-    # print(train_data[["Class1.1","Class1.2","Class1.3"]].values)
-    # print(np.argmax(train_data[["Class1.1","Class1.2","Class1.3"]].values,axis=1))
-    para = train_data[["Class1.1", "Class1.2", "Class1.3"]].values
-    train_data["Category"] = np.argmax(para, axis=1)
-    train_data["Confidence"] = np.max(para, axis=1) * 2 + np.min(para, axis=1) - 1
-    train_data = train_data[train_data["Confidence"] > 0.9]
-    print(train_data["Category"].value_counts())
-    return train_data
-
-
 def concat(raw):
     return np.array([i for i in raw.values])
 
 
 def balanced_select(raw_data, size):
-    one_featured = raw_data.loc[raw_data['Class'] == 1][:size]
-    zero_feature = raw_data.loc[raw_data['Class'] == 0][:size]
-    return pd.concat([one_featured, zero_feature])
+    one_feature = raw_data[raw_data['Class'] == 1]
+    print(one_feature)
+    zero_feature = raw_data[raw_data['Class'] == 0][:size]
+    return pd.concat([one_feature, zero_feature])
 
 
-def combine(base_url, csv_url):
-    # classes = process(csv_url)
-    # combined_data = {}
-    # column_name = ["GalaxyID"]
-    # column_name.extend("Pixel%d" % i for i in range(64*64))
-    # j = 0
-    # for image in sorted(os.listdir(base_url)):
-    #     Id = int(image[:len(image) - 4])
-    #     if Id not in classes["GalaxyID"].values:
-    #         continue
-    #     img_path = os.path.join(base_url, image)
-    #     img_data = [Id]
-    #     src = cv2.resize(cv2.imread(img_path),(64,64))
-    #     img = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-    #     img = img.flatten() / 255.0
-    #     # cv2.namedWindow("gray-image")
-    #     # cv2.imshow("gray-image",img)
-    #     # cv2.waitKey()
-    #     img_data.extend(img)
-    #     j += 1
-    #     print("Round {}".format(j))
-    #     # print(len(img_data))
-    #     for i in range(len(img_data)):
-    #         if column_name[i] not in combined_data.keys():
-    #             combined_data.setdefault(column_name[i], [])
-    #         combined_data[column_name[i]].append(img_data[i])
-    #     if "Category" not in combined_data.keys():
-    #         combined_data.setdefault("Category",[])
-    #     if "Confidence" not in combined_data.keys():
-    #         combined_data.setdefault("Confidence",[])
-    #     current=classes[classes["GalaxyID"].values == Id]
-    #     category=current["Category"].values[0]
-    #     confidence=current["Confidence"].values[0]
-    #     combined_data["Category"].append(category)
-    #     combined_data["Confidence"].append(confidence)
-    # data_frame=pd.DataFrame(combined_data)
-    # data_frame.to_csv("images.csv")
-    print(pd.read_csv("solutions.csv")["Class3.1"].head())
-    print(pd.read_csv("solutions.csv")["Class3.2"].head())
+def standardize(raw_data):
+    return (raw_data - raw_data.mean()) / raw_data.std()
 
 
 def select(base_url):
     data_frame = read(base_url)
     # print(data_frame.head())
     data_frame = data_frame.sample(frac=1)
-    train_num, val_num = int(0.7 * len(data_frame)), int(0.2 * len(data_frame))
-    train_csv = data_frame[:train_num]
-    val_csv = data_frame[train_num:(train_num + val_num)]
-    test_csv = data_frame.iloc[(train_num + val_num):]
-    train_csv.to_csv("train.csv", index=False)
-    val_csv.to_csv("val.csv", index=False)
-    test_csv.to_csv("test.csv", index=False)
+    print(data_frame.head())
+    # print(data_frame.head())
+    data_frame=balanced_select(data_frame,20000)
+    print(data_frame.head())
+    # data_frame.to_csv("train.csv")
+    # train_num, val_num = int(0.7 * len(data_frame)), int(0.2 * len(data_frame))
+    # train_csv = data_frame[:train_num]
+    # val_csv = data_frame[train_num:(train_num + val_num)]
+    # test_csv = data_frame.iloc[(train_num + val_num):]
+    # train_csv.to_csv("train.csv", index=False)
+    # val_csv.to_csv("val.csv", index=False)
+    # test_csv.to_csv("test.csv", index=False)
