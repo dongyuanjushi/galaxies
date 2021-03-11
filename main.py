e759d6bc59acae9265d5981238893ab791251506
dongyuanjushi <1978349625@qq.com>
2021-03-11 10:19:33 +0800

    Program entry

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from util.select import select
 from model.supervised_learning import s_execute
-
 from model.active_learning import a_execute
 
 if __name__ == '__main__':
     base_url = "galaxies.csv"
     train_url = "train.csv"
+    
     # data shuffle and preprocess
     # select(base_url)
 
